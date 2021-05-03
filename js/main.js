66752a15e86213c9c98bb56672f288ba4cab1824
eude313 <eudes.charles@student.moringaschool.com>
2021-05-03 17:18:04 +0300

    remove files

--- js/main.js
+++ js/main.js
@@ -1,13 +1,25 @@
-var dailyProduction = [{ "shedName": "Shed A", "Amount": 200 }];
+var dailySale = [{ "shedName": "Shed A", "amount": 200 }];
 
 function sendData() {
     var shedName = document.getElementById("shedName").value;
     var amount = document.getElementById("amount").value;
-    var data = { "shedName": shedName, "Amount": amount };
-    dailyProduction.push(data);
+    var data = { "shedName": shedName, "amount": amount };
+    dailySale.push(data);
     document.getElementById("addform").reset();
 }
 
-/*function getShedSales() {
-    document.getElementById("details").innerHTML = "";
-}*/
+function getProduction() {
+    var table = document.getElementById("data");
+    table.innerHTML = "";
+    var dat = "";
+    dailySale.forEach(x => {
+        dat += '<tr>';
+        dat += '<td>' + 'Production' + x.shedName + '</td>' + '<td>' + x.amount + ' lts/day' + '</td>';
+        dat += '</tr>';
+    });
+    var sum = dailySale.reduce(function(total, shedAmount) {
+        return total + shedAmount.amount;
+    }, 0);
+    dat += "<tr style='font-weight:bold';><td> Total</td><td>" + sum + " litres per day</td><td></td></tr>";
+    table.innerHTML += dat;
+}
